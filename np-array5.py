b4950568c0c4c02318082982e9575f4bab838a2f
Satiro07 <jardelsatiro57@gmail.com>
2025-04-06 09:43:27 -0300

    questoes np.array

--- np-array5.py
+++ np-array5.py
@@ -6,7 +6,7 @@ array = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
 print(f'Array: {array}')
 
 pos = 5
-print(f'Número na posição {pos}: {array[5]}')
+print(f'Número na posição {pos}: {array[pos]}')
 
 inicio = 10
 fim = 15
