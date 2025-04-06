cd1a461787993d51e74008e093df3a36389f19db
Satiro07 <jardelsatiro57@gmail.com>
2025-04-06 08:25:22 -0300

    questoes np.array

---
+++ np-array3.py
@@ -0,0 +1,11 @@
+# somar elemento por elemento de 2 arrays
+
+import numpy as np
+
+array1 = np.array([1, 2, 3])
+print(f'Array 1: {array1}')
+
+array2 = np.array([4, 5, 6])
+print(f'Array 2: {array2}')
+
+print(f'Soma de elemento por elemento: {array1+array2}')
