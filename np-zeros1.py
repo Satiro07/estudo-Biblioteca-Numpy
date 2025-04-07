82f204b4bacb3f21c3377de2af67e3f692ca1f67
Satiro07 <jardelsatiro57@gmail.com>
2025-04-07 18:50:51 -0300

    Update np-zeros1.py

--- np-zeros1.py
+++ np-zeros1.py
@@ -1,1 +1,13 @@
+import numpy as np 
+array = np.zeros((10, 10))
+print('Array inicial:')
+print(array)
 
+for i in range(0, len(array)):
+
+    array[0] = 255
+    array[i][0] = 255
+    array[i][-1] = 255
+    array[-1] = 255
+print('Array modificada:')
+print(array)
