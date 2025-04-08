ac7b7e7fa3fc9e4e94076ca7b97a303428e73a1e
Satiro07 <jardelsatiro57@gmail.com>
2025-04-08 19:58:04 -0300

    .

--- np-arange1.py
+++ np-arange1.py
@@ -1,6 +1,7 @@
-
 import numpy as np
-array = np.arange(0, 21, 5)
-array = array.reshape(5*5)
 
-print(array)
+array_p = np.ones(int(5, 5))
+array = np.arange(0, 21, 5)
+for i in range(0, len(array_p)):
+    array_p[i] = array
+print(array_p)
---
+++ np-arange2.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+array = np.arange(0, 10.01, 0.5)
+soma_array = np.sum(array)
+print(soma_array)
