fc024d9a2d61835d0cdfc6bd11941ba3caff31c8
Satiro07 <jardelsatiro57@gmail.com>
2025-04-10 18:57:28 -0300

    Create fatiamento-np.py

---
+++ fatiamento-np.py
@@ -0,0 +1,15 @@
+# cortar um array em 'dois pedaços'
+import numpy as np
+array = np.arange(0, 20)
+quantidade = len(array)/2
+pedaco1 = array[0: int(quantidade)]
+pedaco2 = array[int(quantidade):]
+print(array)
+print()
+print('Primeiro pedaço')
+print()
+print(pedaco1)
+print()
+print('Segundo pedaço')
+print()
+print(pedaco2)
