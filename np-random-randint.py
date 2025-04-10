774b936c572e888b8222bdd5ecf0669a2fba82be
Satiro07 <jardelsatiro57@gmail.com>
2025-04-09 22:30:39 -0300

    Update np-random-randint.py

--- np-random-randint.py
+++ np-random-randint.py
@@ -1,6 +1,6 @@
 # gerar 20 números aleatorios de 1 a 100, encontrar maior, minímo ,soma e a media
 import numpy as np 
-array = np.random.randint(1, 100, size=(1,20))
+array = np.random.randint(1, 100, size=(20))
 maior = np.max(array)
 menor = np.min(array)
 soma = np.sum(array)
