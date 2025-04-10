0354c7ab0b5510d4bfec081c2248d74a46086ba0
Satiro07 <jardelsatiro57@gmail.com>
2025-04-09 21:35:30 -0300

    Update np-random-randint1.py

--- np-random-randint1.py
+++ np-random-randint1.py
@@ -3,7 +3,14 @@ import numpy as np
 array = np.random.randint(0, 10, size=(5, 10))
 cont = 1
 print(array)
+nome_maior = ''
+media_maior = 0
 for i in array:
     media = np.mean(i)
     print(f'Aluno {cont}: {media}')
+    if media > media_maior:
+        nome_maior = (f'Aluno {cont}')
+        media_maior = media
     cont += 1
+print()
+print(f'{nome_maior} (média: {media_maior})')
