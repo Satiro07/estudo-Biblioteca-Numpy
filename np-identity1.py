e88e366a30bd91b887d2776aaaec2e908fbae2cc
Satiro07 <jardelsatiro57@gmail.com>
2025-04-10 17:24:33 -0300

    Update np-identity1.py

--- np-identity1.py
+++ np-identity1.py
@@ -4,7 +4,7 @@ array = np.arange(1, 10)
 array = array.reshape(3, 3)
 print(array)
 print()
-array_inv = (array/array) *- 1
+array_inv = array ** (1)
 print(array_inv)
 print()
 produto = array * array_inv
