1a3653ca30ccd153f7738317316488d82b6a3ee6
Satiro07 <jardelsatiro57@gmail.com>
2025-04-09 21:44:41 -0300

    Update np-random-randint2.py

--- np-random-randint2.py
+++ np-random-randint2.py
@@ -1,10 +1,10 @@
-# criar duas matrizes e somar as matrizes
+# criar duas matrizes e somar as matrizes (formando uma matriz final)
 import numpy as np
 matriz1 = np.random.randint(1, 50, size=(3, 3))
-print(matriz1)
+print(f'Matriz 1: \n{matriz1}')
 print()
 matriz2 = np.random.randint(1, 50, size=(3, 3))
-print(matriz2)
-soma_m = np.sum(matriz1,matriz2)
+print(f'Matriz 2: \n{matriz2}')
+soma_m = matriz1 + matriz2
 print()
-print(f'Soma entre as matrizes 1 e 2: {soma_m}')
+print(f'Soma entre as matrizes 1 e 2: \n{soma_m}')
