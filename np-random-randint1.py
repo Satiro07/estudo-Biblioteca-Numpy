aa191d1b6628859222750dd40bf0ecf5fe8ba7ba
Satiro07 <jardelsatiro57@gmail.com>
2025-04-09 21:31:44 -0300

    Update np-random-randint1.py

--- np-random-randint1.py
+++ np-random-randint1.py
@@ -1,4 +1,9 @@
-
+# 'simulação' de dados, media cada aluno, aluno com maior nota
 import numpy as np
 array = np.random.randint(0, 10, size=(5, 10))
+cont = 1
 print(array)
+for i in array:
+    media = np.mean(i)
+    print(f'Aluno {cont}: {media}')
+    cont += 1
