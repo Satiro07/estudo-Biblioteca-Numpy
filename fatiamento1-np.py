3a2ef0a3cf019dbbecea7d61749675959fa1b52f
Satiro07 <jardelsatiro57@gmail.com>
2025-04-10 20:13:40 -0300

    Update fatiamento1-np.py

--- fatiamento1-np.py
+++ fatiamento1-np.py
@@ -2,9 +2,14 @@
 
 import numpy as np
 array = np.random.randint(1, 21, size=(5,5))
+print('Array original')
+print()
 print(array)
+print()
 cont = 0
 for i in array:
     i[cont] = 99
     cont += 1
+print('Array modificada')
+print()
 print(array)
