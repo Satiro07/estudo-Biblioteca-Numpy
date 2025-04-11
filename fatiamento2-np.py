4a1117b782ebd4634748c50a8874842d62f5bd2b
Satiro07 <jardelsatiro57@gmail.com>
2025-04-10 21:00:26 -0300

    Update fatiamento2-np.py

--- fatiamento2-np.py
+++ fatiamento2-np.py
@@ -1,5 +1,12 @@
 
 import numpy as np
 lista = ['ana', 'bruno', 'carlos', 'daniela', 'eduardo']
+print('Lista original')
+print()
+print(lista)
+print()
 array = np.array(lista[::-1])
+print('Lista invertida')
+print()
 print(array)
+print()
