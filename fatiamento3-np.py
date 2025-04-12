01f1bf56bda74388e0dabc49eb69af2645f6d93c
Satiro07 <jardelsatiro57@gmail.com>
2025-04-11 21:32:48 -0300

    Update fatiamento3-np.py

--- fatiamento3-np.py
+++ fatiamento3-np.py
@@ -3,12 +3,13 @@ import numpy as np
 array = np.random.randint(1, 37, size=(9, 9))
 x = int(input('Valor x: '))
 y = int(input('Valor y: '))
+print(array)
 array_g = np.array([])
 auxi = []
 for i in range(0, 1):
     
     for i in range(0, x):
-        aux = [i for i in array[x-1][y:y+y]]
+        aux = [i for i in array[x-1][x:y]]
         auxi.append(aux)
         x += 1
     array_g = np.array(auxi)
