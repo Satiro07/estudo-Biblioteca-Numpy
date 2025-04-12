bf47cc4dfea22684773d62f1da04ef614c7cb00c
Satiro07 <jardelsatiro57@gmail.com>
2025-04-11 21:30:27 -0300

    Update fatiamento3-np.py

--- fatiamento3-np.py
+++ fatiamento3-np.py
@@ -1,12 +1,6 @@
 
 import numpy as np
 array = np.random.randint(1, 37, size=(9, 9))
-print(array)
-coluna = (len(array)// 2)-1
-print(coluna)
-
-linha = (len(array[0])//2)-1
-print(linha)
 x = int(input('Valor x: '))
 y = int(input('Valor y: '))
 array_g = np.array([])
