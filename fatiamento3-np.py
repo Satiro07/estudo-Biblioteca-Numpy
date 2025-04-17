caccabe3b161850c245d2a65ee2a926ca913d750
Satiro07 <jardelsatiro57@gmail.com>
2025-04-17 10:17:05 -0300

    metodos

--- fatiamento3-np.py
+++ fatiamento3-np.py
@@ -2,7 +2,8 @@
 import numpy as np
 array = np.arange(1, 82).reshape(9,9)
 print(array)
+print(np.diag(array))
 
-x = int(input('Valor x: '))
-y = int(input('Valor y: '))
-print(array[x:y+1,x:y+1])
+# x = int(input('Valor x: '))
+# y = int(input('Valor y: '))
+# print(array[x:y+1,x:y+1])
---
+++ metodos_numpy.py
@@ -0,0 +1,5 @@
+import numpy as np
+array = np.arange(1, 82).reshape(9,9)
+print(array)
+print(np.diag(array)) # pega a diagonal de uma array
+print(np.flip(array))
