b0908379ca57ef7e597d739d4de11aefa7c0b3db
Satiro07 <jardelsatiro57@gmail.com>
2025-04-17 10:08:28 -0300

    Update fatiamento3-np.py

--- fatiamento3-np.py
+++ fatiamento3-np.py
@@ -2,8 +2,7 @@
 import numpy as np
 array = np.arange(1, 82).reshape(9,9)
 print(array)
-print(array[1:3,1:5])
 
-
-# x = int(input('Valor x: '))
-# y = int(input('Valor y: '))
+x = int(input('Valor x: '))
+y = int(input('Valor y: '))
+print(array[x:y+1,x:y+1])
