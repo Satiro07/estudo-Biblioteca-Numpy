3966def36f764bd402e182b2ee9b42f241af4270
Satiro07 <jardelsatiro57@gmail.com>
2025-04-17 10:04:50 -0300

    Update fatiamento3-np.py

--- fatiamento3-np.py
+++ fatiamento3-np.py
@@ -1,7 +1,8 @@
 
-# import numpy as np
-# array = np.arange(1, 82).reshape(9,9)
-# print(array)
+import numpy as np
+array = np.arange(1, 82).reshape(9,9)
+print(array)
+print(array[1:3,1:5])
 
 
 # x = int(input('Valor x: '))
