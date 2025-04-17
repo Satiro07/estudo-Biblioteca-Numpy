9eac46dc1ae06199a8504767fdbbd31a552e3400
Satiro07 <jardelsatiro57@gmail.com>
2025-04-17 11:19:03 -0300

    Update metodos_numpy.py

--- metodos_numpy.py
+++ metodos_numpy.py
@@ -1,5 +1,10 @@
 import numpy as np
 array = np.arange(1, 82).reshape(9,9)
 print(array)
-print(np.diag(array)) # pega a diagonal de uma array
-print(np.flip(array))
+print(np.diag(array)) # pega a diagonal de um array
+print(np.flip(array)) # inverte o array
+divi = np.split(array, 3) # divide o array
+print(divi)
+array1 = np.arange(1, 10)
+array2 = np.arange(5, 10)
+print(np.concatenate((array1, array2)))
