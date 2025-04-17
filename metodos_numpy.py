94c7baf316d9d653a76975edae194fb4f6139872
Satiro07 <jardelsatiro57@gmail.com>
2025-04-17 11:28:36 -0300

    Update metodos_numpy.py

--- metodos_numpy.py
+++ metodos_numpy.py
@@ -9,3 +9,6 @@ array1 = np.arange(1, 10)
 array2 = np.arange(5, 10)
 print(np.concatenate((array1, array2))) # junta arrays
 print(np.ravel(array)) # transforma um array com várias dimensões em um com apenas uma dimensão
+
+axis=0 # vertical
+axis=1 # horizontal
