e24e9f482a1b35262007528cb0fd46702cc89a92
Satiro07 <jardelsatiro57@gmail.com>
2025-06-14 21:11:58 -0300

    Delete vetorizacao.py

--- fundamentos_vetorizacao/vetorizacao.py
+++
@@ -1,8 +0,0 @@
-from sklearn.feature_extraction.text import CountVectorizer
-
-frases = ["gostei do produto", "não gostei", "produto ruim"]
-vetorizador = CountVectorizer() # aprende as palavraas que existem no conjunto de frases
-X = vetorizador.fit_transform(frases) # aprendizado + transformação fit() todas as palavras únicas, transform() tranforma cada frase em um vetor de contagem
-
-print(vetorizador.get_feature_names_out())
-print(X.toarray())
