bac5297632cce4e20e950c7d8efd4992aba1c82e
Satiro07 <jardelsatiro57@gmail.com>
2025-06-15 20:41:26 -0300

    Create np-ones.py

---
+++ estudo_numpy/np-ones.py
@@ -0,0 +1,12 @@
+# multiplicando cada linha
+import numpy as np
+
+
+array = np.ones((4, 4))
+print('Matriz inicial')
+print(array)
+print()
+for i in range(0, len(array)):
+    array[i] *= i+1
+print('Matriz final')
+print(array)
