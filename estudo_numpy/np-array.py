9d80a5972dd15d78b733fe66afd720b615caa9a0
Satiro07 <jardelsatiro57@gmail.com>
2025-06-15 20:40:56 -0300

    Create np-array.py

---
+++ estudo_numpy/np-array.py
@@ -0,0 +1,6 @@
+# calcular soma dos elementos de um array
+import numpy as np
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+array = np.array(lista)
+print(f'Array: {array}')
+print(f'Soma do array: {sum(array)}')
